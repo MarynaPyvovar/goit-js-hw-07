0b1c08e2881fd5352c69cb526f30d758a9daa9b9
MarynaPyvovar <marina18renkas@gmail.com>
2022-07-11 20:02:31 +0300

    feat 02-gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -11,14 +11,26 @@ function createImagesMarkup(items) {
     return items
         .map(({ preview, original, description, }) => {
         return `<li>
-    <a class="gallery__link" href="${original}">
-    <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-    />
-    </a>
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
 </li>`})
         .join('');
 };
+
+gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+    const isImage = event.target.classList.contains('gallery__image');
+    if (!isImage) return;
+
+    event.preventDefault();
+
+    var lightbox = new SimpleLightbox('.gallery a', {
+        captionAttribute: 'alt',
+        urlAttribute: 'href',
+        loadingTimeout: 250,
+    });
+
+    return lightbox
+}
